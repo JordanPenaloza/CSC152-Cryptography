842fe5a16473ebab8b674577e4b9530ecd6f7fd0
jordanpenaloza <jordan332033@gmail.com>
2024-09-06 13:25:10 -0700

    added bitstoflip.c

---
+++ Assignments/bitstoflip.c
@@ -0,0 +1,10 @@
+int bits_to_flip (int a, int b) {
+    int c = a ^ b;
+    int count = 0;
+    for (int i = 0; i <= 31; i ++) {
+        count += c & 1;
+        c >>= 1;
+    }
+    return count; 
+}
+
