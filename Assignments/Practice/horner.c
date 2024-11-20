8c448cc1a7883d8a1a9e988aacc73a7e86fc6768
JordanPenaloza <jordan332033@gmail.com>
2024-11-19 20:21:57 -0800

    horner method

---
+++ Assignments/Practice/horner.c
@@ -0,0 +1,17 @@
+/*
+ (5 Pts) Write a C function using Horners method to compute and return kn +a1kn−1+a2kn−2+...+ ank) mod p. 
+ Parameter a points to an array of n elements. 
+ All elements in a and the value k are nonnegative and less than p. 
+ Do not use divisionless mod in this problem. uint32_t poly(uint32_t *a, int n, uint32_t k, uint32_t p) {
+
+*/
+
+uint32_t poly(uint32_t *a, int n, uint32_t k, uint32_t p) {
+    uint64t acc = k; //to mitigate buffer overflow use 64 instead of 32
+    for(int i = 0; i < n; i++){
+        acc = acc * k;
+        acc = acc + a[i];
+        acc = acc % p;
+    }
+    return (uint32_t)acc;
+}
