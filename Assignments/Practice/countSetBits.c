6eec2409eb892451170f07ec82a736a858b4755d
JordanPenaloza <jordan332033@gmail.com>
2024-11-19 20:51:20 -0800

    count set bits practice

---
+++ Assignments/Practice/countSetBits.c
@@ -0,0 +1,10 @@
+int countSetBits(uint32_t x){
+    int count = 0;
+    for(int i = 0; i < 32; i++){
+        if((x & 1) == 1) {
+            count++;
+        }
+        x >>= 1;
+    }
+    return count;
+}
